b506730dfb88e8be7df1723ddec082da788c5928
Jérôme GIACOMINI <archeone@gmail.com>
2018-08-27 12:21:56 +0200

    Add overload on ExecuteAsync with less parameters

--- TinyHttp/Request/IExecutableRequest.cs
+++ TinyHttp/Request/IExecutableRequest.cs
@@ -23,7 +23,7 @@ namespace Tiny.Http
         /// <param name="formatter">Allow to override the formatter use for the deserialization.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>Task of TResukt</returns>
-        Task<TResult> ExecuteAsync<TResult>(IFormatter formatter = null, CancellationToken cancellationToken = default);
+        Task<TResult> ExecuteAsync<TResult>(IFormatter formatter, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the request.
