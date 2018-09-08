4995a58b6d177851c17ec88468e5a381d7ff1d67
Jérôme GIACOMINI <archeone@gmail.com>
2018-09-08 13:33:18 +0200

    Add doc

--- TinyHttp/Listerner/DebugListernerExtension.cs
+++ TinyHttp/Listerner/DebugListernerExtension.cs
@@ -1,7 +1,15 @@
 ﻿namespace Tiny.Http
 {
+    /// <summary>
+    /// Extension to add easily debug listener
+    /// </summary>
     public static class DebugListernerExtension
     {
+        /// <summary>
+        /// Add <see cref="DebugListener" to listeners />
+        /// </summary>
+        /// <param name="listeners">all listeners</param>
+        /// <param name="measureTime">allow to measure time to this listener</param>
         public static void AddDebug(this Listeners listeners, bool measureTime = true)
         {
             listeners.Add(new DebugListener(measureTime));
