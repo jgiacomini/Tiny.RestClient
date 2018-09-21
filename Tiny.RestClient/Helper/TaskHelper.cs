b82e414c979a6f7a6cdbbe80c68ac9a105ea9bd3
Jérôme GIACOMINI <archeone@gmail.com>
2018-09-21 14:38:41 +0200

    start of support of net45

---
+++ Tiny.RestClient/Helper/TaskHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Tiny.RestClient
+{
+    internal class TaskHelper
+    {
+        private static Task _completedTask;
+
+        ///  <summary>Gets a task that's already been completed successfully.</summary>
+        ///  <remarks>May not always return the same instance.</remarks>
+        public static Task CompletedTask
+        {
+            get
+            {
+#if NETFX_45
+                var completedTask = _completedTask;
+                if (completedTask == null)
+                {
+                    _completedTask = (Task)Task.FromResult(false);
+                }
+
+                return _completedTask;
+#else
+                return Task.CompletedTask;
+#endif
+
+            }
+        }
+    }
+}
