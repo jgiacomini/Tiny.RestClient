375aa758b2bc512da96c624b2ed0d7c513e920bb
Jérôme GIACOMINI <archeone@gmail.com>
2018-09-22 17:34:59 +0200

    change a little the tests

--- Tests/Tiny.RestClient.Tests/GzipTests.cs
+++ Tests/Tiny.RestClient.Tests/GzipTests.cs
@@ -15,29 +15,22 @@ namespace Tiny.RestClient.Tests
 
             var compression = client.Settings.Compressions["gzip"];
             compression.AddAcceptEncodingHeader = true;
-
-            var postman = client.Settings.Listeners.AddPostman("gzip");
             var data = await client.
                 GetRequest("GetTest/Complex").
                 ExecuteAsync<string[]>();
             Assert.AreEqual(data.Length, 2);
             Assert.AreEqual(data[0], "value1");
             Assert.AreEqual(data[1], "value2");
-
-            await postman.SaveAsync(new FileInfo("gzip.json"));
         }
 
         [TestMethod]
         public async Task NoResponseGzip()
         {
             var client = GetClient();
-            var postman = client.Settings.Listeners.AddPostman("gzip");
             var data = await client.
                 GetRequest("GetTest/noResponse").
                 AddHeader("Accept-Encoding", "gzip").
                 ExecuteAsStringAsync();
-
-            await postman.SaveAsync(new FileInfo("gzip.json"));
         }
     }
 }
