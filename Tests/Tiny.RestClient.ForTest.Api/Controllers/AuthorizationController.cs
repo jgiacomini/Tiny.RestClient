2016f7cdfda45513b5018c348b3c6df88e2cd451
Jérôme GIACOMINI <archeone@gmail.com>
2018-09-23 12:13:17 +0200

    Add tests for Authorizations

---
+++ Tests/Tiny.RestClient.ForTest.Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Tiny.RestClient.ForTest.Api.Filter;
+
+namespace Tiny.RestClient.ForTest.Api.Controllers
+{
+    [Route("api/Authorization")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet("BasicAuthentication")]
+        [BasicAuthorizationAttribute]
+        public Task BasicAuthentication()
+        {
+            return Task.Delay(0);
+        }
+
+        [HttpGet("BearerAuthentication")]
+        public ActionResult BearerAuthentication()
+        {
+            var authorization = Request.Headers["Authorization"].FirstOrDefault();
+            if (authorization == null)
+            {
+                return Unauthorized();
+            }
+
+            return StatusCode(200);
+        }
+    }
+}
---
+++ Tests/Tiny.RestClient.Tests/AuthorizationTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Tiny.RestClient.Tests
+{
+    [TestClass]
+    public class AuthorizationTests : BaseTest
+    {
+        [TestMethod]
+        public async Task BasicAuthorizationWithUnAuthorizedTestAsync()
+        {
+            bool isThrowed = false;
+            try
+            {
+                var client = GetClient();
+                await client.
+                    GetRequest("Authorization/BasicAuthentication").
+                    ExecuteAsync();
+            }
+            catch (HttpException ex) when (ex.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                isThrowed = true;
+            }
+
+            Assert.IsTrue(isThrowed, $"The API must throw exception with StatusCode : {HttpStatusCode.Unauthorized}");
+        }
+
+        [TestMethod]
+        public async Task BasicAuthorizationTestAsync()
+        {
+            var client = GetClient();
+            await client.
+                GetRequest("Authorization/BasicAuthentication").
+                WithBasicAuthentication("username", "42").
+                ExecuteAsync();
+        }
+
+        [TestMethod]
+        public async Task BasicAuthorizationWithDefaultHeadersTestAsync()
+        {
+            var client = GetNewClient();
+            client.Settings.DefaultHeaders.AddBasicAuthentication("username", "42");
+            await client.
+                GetRequest("Authorization/BasicAuthentication").
+                ExecuteAsync();
+        }
+
+        [TestMethod]
+        public async Task BearerAuthorizationWithUnAuthorizedTestAsync()
+        {
+            bool isThrowed = false;
+            try
+            {
+                var client = GetClient();
+                await client.
+                    GetRequest("Authorization/BearerAuthentication").
+                    ExecuteAsync();
+            }
+            catch (HttpException ex) when (ex.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                isThrowed = true;
+            }
+
+            Assert.IsTrue(isThrowed, $"The API must throw exception with StatusCode : {HttpStatusCode.Unauthorized}");
+        }
+
+        [TestMethod]
+        public async Task BearerAuthorizationTestAsync()
+        {
+            var client = GetClient();
+            await client.
+                GetRequest("Authorization/BearerAuthentication").
+                WithOAuthBearer(Guid.Empty.ToString()).
+                ExecuteAsync();
+        }
+
+        [TestMethod]
+        public async Task BearerAuthorizationWithDefaultHeadersTestAsync()
+        {
+            var client = GetNewClient();
+            client.Settings.DefaultHeaders.AddBearer(Guid.Empty.ToString());
+            await client.
+                GetRequest("Authorization/BearerAuthentication").
+                ExecuteAsync();
+        }
+    }
+}
