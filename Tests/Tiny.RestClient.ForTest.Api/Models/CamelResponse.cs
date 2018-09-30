b39cacd662df27dc872b395083de96f7a90c8e09
Jérôme GIACOMINI <archeone@gmail.com>
2018-09-30 14:30:50 +0200

    Tests for CamelCase / KebabCase / SnakeCase

---
+++ Tests/Tiny.RestClient.ForTest.Api/Models/CamelResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Tiny.RestClient.Tests.Models
+{
+    public class CamelResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("responseData")]
+        public string ResponseData { get; set; }
+    }
+}
---
+++ Tests/Tiny.RestClient.Tests/CaseTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+using Tiny.RestClient.Tests.Models;
+
+namespace Tiny.RestClient.Tests
+{
+    [TestClass]
+    public class CaseTests : BaseTest
+    {
+        [TestMethod]
+        public async Task KebabTest()
+        {
+            var client = GetNewClient();
+            client.Settings.Formatters.OfType<JsonFormatter>().First().UseKebabCase();
+
+            var rep = await client.
+                GetRequest("case/Kebab").
+                ExecuteAsync<Response>();
+
+            Assert.AreEqual(rep.Id, 42);
+            Assert.AreEqual(rep.ResponseData, "REP");
+        }
+
+        [TestMethod]
+        public async Task CamelTest()
+        {
+            var client = GetNewClient();
+            client.Settings.Formatters.OfType<JsonFormatter>().First().UseCamelCase();
+
+            var rep = await client.
+                GetRequest("case/Camel").
+                ExecuteAsync<Response>();
+            Assert.AreEqual(rep.Id, 42);
+            Assert.AreEqual(rep.ResponseData, "REP");
+        }
+
+        [TestMethod]
+        public async Task SnakeTest()
+        {
+            var client = GetNewClient();
+            client.Settings.Formatters.OfType<JsonFormatter>().First().UseSnakeCase();
+
+            var rep = await client.
+                GetRequest("case/Snake").
+                ExecuteAsync<Response>();
+            Assert.AreEqual(rep.Id, 42);
+            Assert.AreEqual(rep.ResponseData, "REP");
+        }
+    }
+}
