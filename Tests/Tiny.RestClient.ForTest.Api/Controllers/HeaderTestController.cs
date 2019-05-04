9a5d461f0eca03dc0a6100de8b55476176a87ccb
Jérôme GIACOMINI <jgiacomini@infinitesquare.com>
2019-05-04 17:40:21 +0200

    Add new tests

---
+++ Tests/Tiny.RestClient.ForTest.Api/Controllers/HeaderTestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tiny.RestClient.ForTest.Api.Controllers
+{
+    public class HeaderTestController : Controller
+    {
+        public IActionResult Get()
+        {
+            Response.Headers.Add("CustomHeader", "CustomValue");
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Tests/Tiny.RestClient.Tests/HttpExceptionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiny.RestClient.Tests
+{
+    [TestClass]
+    public class HttpExceptionTests : BaseTest
+    {
+        [TestMethod]
+        public async Task CheckIfHttpExceptionReadHeaders()
+        {
+            bool exceptionThrowed = false;
+            var client = GetClient();
+            try
+            {
+                await client.GetRequest("HeaderTest/Get").ExecuteAsync();
+            }
+            catch (HttpException ex)
+            {
+                exceptionThrowed = true;
+
+                Assert.IsTrue(ex.ResponseHeaders.Contains("CustomHeader"), "An header name 'CustomHeader' must be present in response");
+            }
+
+            Assert.IsTrue(exceptionThrowed, $"An {nameof(HttpException)} must be throwed");
+        }
+    }
+}
