4884deac7b6d0714b0c798010a30c358aa9cc6fa
Jérôme GIACOMINI <archeone@gmail.com>
2019-05-04 20:19:28 +0200

    fix comment

--- Tiny.RestClient/Listener/PostmanListener/PostmanListenerExtension.cs
+++ Tiny.RestClient/Listener/PostmanListener/PostmanListenerExtension.cs
@@ -1,16 +1,16 @@
 ﻿namespace Tiny.RestClient
 {
     /// <summary>
-    /// Extension to add easily postman listener
+    /// Extension to add easily postman listener.
     /// </summary>
     public static class PostmanListenerExtension
     {
         /// <summary>
-        /// Add <see cref="PostmanListener"/> to listeners />
+        /// Add <see cref="PostmanListener"/> to listeners />.
         /// </summary>
-        /// <param name="listeners">all listeners</param>
-        /// <param name="name">name of the collection</param>
-        /// <returns>listener created</returns>
+        /// <param name="listeners">all listeners.</param>
+        /// <param name="name">name of the collection.</param>
+        /// <returns>listener created.</returns>
         public static PostmanListener AddPostman(this Listeners listeners, string name)
         {
             var listener = new PostmanListener(name);
