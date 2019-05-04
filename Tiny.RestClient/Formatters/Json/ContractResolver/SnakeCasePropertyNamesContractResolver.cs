f80117dfe5d997ec6f2c33aff1828dbe5ca82548
Jérôme GIACOMINI <archeone@gmail.com>
2019-05-04 20:16:31 +0200

    fix comment

--- Tiny.RestClient/Formatters/Json/ContractResolver/SnakeCasePropertyNamesContractResolver.cs
+++ Tiny.RestClient/Formatters/Json/ContractResolver/SnakeCasePropertyNamesContractResolver.cs
@@ -1,13 +1,13 @@
 ﻿namespace Tiny.RestClient.Json
 {
     /// <summary>
-    ///  Allow snake case resolve of property (property_name become PropertyName)
+    ///  Allow snake case resolve of property (property_name become PropertyName).
     /// </summary>
     /// <seealso cref="IFormatter" />
     public class SnakeCasePropertyNamesContractResolver : SeparatorPropertyNamesContractResolver
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SnakeCasePropertyNamesContractResolver"/>
+        /// Initializes a new instance of the <see cref="SnakeCasePropertyNamesContractResolver"/>.
         /// </summary>
         public SnakeCasePropertyNamesContractResolver()
             : base('_')
