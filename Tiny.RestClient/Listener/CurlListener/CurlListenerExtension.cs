9970b7df2e882e53b608b5aa9ad69be331778077
Jérôme GIACOMINI <archeone@gmail.com>
2019-05-04 20:17:54 +0200

    fix comment

--- Tiny.RestClient/Listener/CurlListener/CurlListenerExtension.cs
+++ Tiny.RestClient/Listener/CurlListener/CurlListenerExtension.cs
@@ -1,15 +1,15 @@
 ﻿namespace Tiny.RestClient
 {
     /// <summary>
-    /// Extension to add easily curl listener
+    /// Extension to add easily curl listener.
     /// </summary>
     public static class CurlListenerExtension
     {
         /// <summary>
-        /// Add <see cref="CurlListener"/> to listeners />
+        /// Add <see cref="CurlListener"/> to listeners />.
         /// </summary>
-        /// <param name="listeners">all listeners</param>
-        /// <returns>listener created</returns>
+        /// <param name="listeners">all listeners.</param>
+        /// <returns>listener created.</returns>
         public static CurlListener AddCurl(this Listeners listeners)
         {
             var listener = new CurlListener();
