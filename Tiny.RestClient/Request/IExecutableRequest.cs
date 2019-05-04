2eae907c101829c4f00524941a6d2e305db70e65
Jérôme GIACOMINI <archeone@gmail.com>
2019-05-04 20:20:18 +0200

    fix comment

--- Tiny.RestClient/Request/IExecutableRequest.cs
+++ Tiny.RestClient/Request/IExecutableRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Tiny.RestClient
 {
     /// <summary>
-    /// Interface IExecutableRequest
+    /// Interface IExecutableRequest.
     /// </summary>
     public interface IExecutableRequest
     {
@@ -15,7 +15,7 @@ namespace Tiny.RestClient
         /// </summary>
         /// <typeparam name="TResult">The type of the TResult.</typeparam>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns><see cref="Task{TResult}"/></returns>
+        /// <returns><see cref="Task{TResult}"/>.</returns>
         Task<TResult> ExecuteAsync<TResult>(CancellationToken cancellationToken = default);
 
         /// <summary>
@@ -24,43 +24,43 @@ namespace Tiny.RestClient
         /// <typeparam name="TResult">The type of the TResult.</typeparam>
         /// <param name="formatter">Allow to override the formatter use for the deserialization.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Task of TResukt</returns>
+        /// <returns>Task of TResukt.</returns>
         Task<TResult> ExecuteAsync<TResult>(IFormatter formatter, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the request.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns><see cref="Task{Stream}"/></returns>
+        /// <returns><see cref="Task{Stream}"/>.</returns>
         Task<Stream> ExecuteAsStreamAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the request.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Task of string</returns>
+        /// <returns>Task of string.</returns>
         Task<string> ExecuteAsStringAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the request.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Task of byte array</returns>
+        /// <returns>Task of byte array.</returns>
         Task<byte[]> ExecuteAsByteArrayAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Executes the request.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Task of <see cref="HttpResponseMessage"/></returns>
+        /// <returns>Task of <see cref="HttpResponseMessage"/>.</returns>
         Task<HttpResponseMessage> ExecuteAsHttpResponseMessageAsync(CancellationToken cancellationToken = default);
 #if !FILEINFO_NOT_SUPPORTED
         /// <summary>
-        /// Download file async
+        /// Download file async.
         /// </summary>
-        /// <param name="path">path to download</param>
+        /// <param name="path">path to download.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>return a file info</returns>
+        /// <returns>return a file info.</returns>
         Task<FileInfo> DownloadFileAsync(string path, CancellationToken cancellationToken = default);
 #endif
 
@@ -68,7 +68,7 @@ namespace Tiny.RestClient
         /// Executes the request.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>A task</returns>
+        /// <returns>A task.</returns>
         Task ExecuteAsync(CancellationToken cancellationToken = default);
     }
 }
