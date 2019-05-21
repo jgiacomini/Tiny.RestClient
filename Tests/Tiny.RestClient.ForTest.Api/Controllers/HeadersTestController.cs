8e9276aefcc67bba276b8aad2add02204fd06da6
Jérôme GIACOMINI <archeone@gmail.com>
2019-05-21 07:02:04 +0200

    Calculate headers before send request (#90)
    Add possibility to calculate headers just before send requests

---
+++ Tests/Tiny.RestClient.ForTest.Api/Controllers/HeadersTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tiny.RestClient.ForTest.Api.Controllers
+{
+    [Route("api/HeadersTest")]
+    [ApiController]
+    public class HeadersTestController : ControllerBase
+    {
+        [HttpGet("NoResponse")]
+        public Task NoResponse()
+        {
+            foreach (var header in Request.Headers)
+            {
+                Response.Headers.Add("FROM_CLIENT" + header.Key, header.Value);
+            }
+
+            return Task.Delay(1);
+        }
+    }
+}
